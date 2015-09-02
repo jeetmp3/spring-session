d1ff8007e96bb0335a565c470da6c170476c1a9a
Jitendra Singh Bisht <jitendra.singh@tothenew.com>
2015-09-02 19:54:57 +0530

    License file added; Issue url added;

--- SpringSessionGrailsPlugin.groovy
+++ SpringSessionGrailsPlugin.groovy
@@ -9,11 +9,8 @@ import org.springframework.session.web.http.HeaderHttpSessionStrategy
 import org.springframework.web.filter.DelegatingFilterProxy
 
 class SpringSessionGrailsPlugin {
-    // the plugin version
     def version = "0.1"
-    // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
-    // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp",
             "grails-app/controllers",
@@ -25,28 +22,15 @@ class SpringSessionGrailsPlugin {
     def author = "Jitendra Singh"
     def authorEmail = "jeet.mp3@gmail.com"
     def description = '''\
-Spring Session Grails Plugin provides support for SpringSession project available in Grails.
+Spring Session Grails Plugin provide support for SpringSession project available in Grails.
 '''
 
-    // URL to the plugin's documentation
-    def documentation = "https://github.com/jeetmp3/spring-session"
-//    def documentation = "http://grails.org/plugin/spring-session"
+    def documentation = "http://grails.org/plugin/spring-session"
 
-    // Extra (optional) plugin metadata
+    def license = "APACHE"
 
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def issueManagement = [ system: "Github", url: "https://github.com/jeetmp3/spring-session/issues" ]
 
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-    // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
-    // Online location of the plugin's browseable source code.
     def scm = [ url: "https://github.com/jeetmp3/spring-session" ]
 
     def loadAfter = ['springSecurityCore', 'cors']
@@ -59,18 +43,37 @@ Spring Session Grails Plugin provides support for SpringSession project availabl
         def contextParams = xml.'context-param'
         contextParams[contextParams.size() - 1] + {
             filter {
+                'filter-name'('httpSessionSynchronizer')
+                'filter-class'(DelegatingFilterProxy.name)
+            }
+        }
+
+        contextParams[contextParams.size() - 1] + {
+            filter {
                 'filter-name'('springSessionRepositoryFilter')
                 'filter-class'(DelegatingFilterProxy.name)
             }
         }
 
-        def filterMapping = xml.'filter-mapping'
-        filterMapping[filterMapping.size() - 1] + {
+//        def filterMapping = xml.'filter-mapping'
+        def filter = xml.'filter'
+
+        filter[filter.size() - 1] + {
+            'filter-mapping' {
+                'filter-name'('httpSessionSynchronizer')
+                'url-pattern'('/*')
+                dispatcher('ERROR')
+                dispatcher('REQUEST')
+            }
+        }
+
+        filter[filter.size() - 1] + {
             'filter-mapping' {
                 'filter-name'('springSessionRepositoryFilter')
                 'url-pattern'('/*')
                 dispatcher('ERROR')
                 dispatcher('REQUEST')
+                dispatcher('FORWARD')
             }
         }
     }
