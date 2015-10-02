74bb804d2c2abfc41f5ec007f8a484832b410bbe
Jitendra Singh Bisht <jitendra.singh@tothenew.com>
2015-10-02 09:44:02 +0530

    Response commit check added in SessionSynchronizer

--- src/java/grails/plugin/springsession/web/http/HttpSessionSynchronizer.java
+++ src/java/grails/plugin/springsession/web/http/HttpSessionSynchronizer.java
@@ -17,14 +17,16 @@ public class HttpSessionSynchronizer extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         filterChain.doFilter(request, response);
-        HttpSession session = request.getSession();
-        Enumeration<String> attributeNames = session.getAttributeNames();
-        while (attributeNames.hasMoreElements()) {
-            String key = attributeNames.nextElement();
-            try {
-                Object object = session.getAttribute(key);
-                session.setAttribute(key, object);
-            } catch (Exception ignored) {
+        if (!response.isCommitted()) {
+            HttpSession session = request.getSession();
+            Enumeration<String> attributeNames = session.getAttributeNames();
+            while (attributeNames.hasMoreElements()) {
+                String key = attributeNames.nextElement();
+                try {
+                    Object object = session.getAttribute(key);
+                    session.setAttribute(key, object);
+                } catch (Exception ignored) {
+                }
             }
         }
     }
