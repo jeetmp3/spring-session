342e51cf707b9bca3f9e0266f3d17986b89e1b5e
Jitendra Singh Bisht <jitendra.singh@tothenew.com>
2016-05-28 12:27:30 +0530

    Tests configured

--- test/integration/ConfigurationTest.groovy
+++ test/integration/ConfigurationTest.groovy
@@ -1,20 +1,13 @@
 import grails.test.spock.IntegrationSpec
 import org.codehaus.groovy.grails.commons.GrailsApplication
-import org.junit.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
-import redis.clients.jedis.JedisShardInfo
 
 /**
  * @author jitendra on 2/10/15.
  */
 class ConfigurationTest extends IntegrationSpec {
-    @Autowired
-    JedisConnectionFactory redisConnectionFactory
-    @Autowired
+
+    def redisConnectionFactory
     GrailsApplication grailsApplication
-    @Autowired
-    JedisShardInfo shardInfo
 
     void setup() {
 
@@ -25,21 +18,11 @@ class ConfigurationTest extends IntegrationSpec {
         true;
     }
 
-    void "Test ShardInfo"(){
-        expect:
-        shardInfo
-    }
-
     def "Test Connection Factory"() {
 
         expect:
         redisConnectionFactory
     }
-//    @Test
-//    void "Test Redis connection Factory"() {
-//        expect:
-//        grailsApplication.mainContext.getBean("redisConnectionFactory")
-//    }
 
     void cleanup() {
 
