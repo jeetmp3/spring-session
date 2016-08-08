51c3b8edf56cbc02cefb6946a071fbc51f394d7d
Jitendra Singh <jeet.mp3@gmail.com>
2016-08-08 20:00:46 +0530

    Spring jdbc and mongo dependencies added

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -12,9 +12,15 @@ grails.project.dependency.resolution = {
     }
 
     dependencies {
+        compile('org.springframework:spring-core:4.1.1.RELEASE')
         compile('org.springframework.session:spring-session:1.2.1.RELEASE')
         compile('org.springframework.data:spring-data-redis:1.4.1.RELEASE')
+        compile('org.springframework.data:spring-data-mongodb:1.8.0.RELEASE')
+        compile('com.fasterxml.jackson.core:jackson-databind:2.8.1')
+        compile('org.apache.commons:commons-dbcp2:2.0')
         compile('redis.clients:jedis:2.5.2')
+
+        provided('com.h2database:h2:1.4.191')
     }
 
     plugins {
