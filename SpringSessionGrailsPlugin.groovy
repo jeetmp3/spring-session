5666334bdace36997475a4ddbc7eb51c4778b7b6
BrunoVieira <brunovieira@zgsolucoes.com.br>
2017-11-13 15:18:25 -0200

    Mensagem: Atualizando o master para o próximo número de versão '1.4-ZG10-SNAPSHOT'

--- SpringSessionGrailsPlugin.groovy
+++ SpringSessionGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import grails.plugin.webxml.FilterManager
 import org.grails.plugins.springsession.SpringSessionGrailsPluginSupport
 
 class SpringSessionGrailsPlugin {
-	def version = '1.4-ZG9'
+	def version = '1.4-ZG10-SNAPSHOT'
 	def grailsVersion = "2.2.4 > *"
 	def title = "Spring Session Grails Plugin"
 	def author = "Jitendra Singh"
